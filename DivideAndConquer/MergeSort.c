d401bb2b8b24ad1b45094a52ff91c75637f151c4
José Kling <jkling@geomar.de>
2024-02-07 16:43:04 +0100

    Improvement of merge sort algorithm
    Instead of creating copies of the 'left' and 'right' arrays every time
    the function 'merge' is called, only one copy of the whole original
    array is created and used throughout the whole algorithm.
    Also trying to make .gitignore work.

--- DivideAndConquer/MergeSort.c
+++ DivideAndConquer/MergeSort.c
@@ -5,59 +5,77 @@ This is an impleentation of merge sort. It asks the user for
 the length of the array to be sorted and the range of values
 allowed in the array. The script prints the original array,
 sorts it and prints it after sorting.
-The sorting occurs in place, but still in every iteration a
-new array has to be created, therefore it needs O(n) memory.
+Instead of creating new arrays every time 'merge_sort' is recursively
+called, the operations are done in place by manipulationg pointers. Also,
+in the 'merge' function, instead of creating copies of the left and right
+sides of the array, only one copy of the original array is created in the
+beginning of the algorithm, and the 'left_copy' and 'right_copy' arrays
+in 'merge' are just pointers to this first copy.
+In this way, there is no need to create copies of the 'left' and 'right'
+arrays in 'merge', we just use the original copy and keep it updated.
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-void merge(int left[], int len_l, int right[], int len_r) {
+void merge(int left[], int left_copy[], int len_l, int right[], int right_copy[], int len_r) {
     int idx_l = 0, idx_r = 0, idx_sorted = 0;
     int sorted[len_l + len_r];
     while ((idx_l < len_l) && (idx_r < len_r)) {
-        if (left[idx_l] <= right[idx_r]) {
-            sorted[idx_sorted] = left[idx_l];
+        if (left_copy[idx_l] <= right_copy[idx_r]) {
+            left[idx_sorted] = left_copy[idx_l];
             idx_l++;
             idx_sorted++;
         } else {
-            sorted[idx_sorted] = right[idx_r];
+            left[idx_sorted] = right_copy[idx_r];
             idx_r++;
             idx_sorted++;
         }
     }
     if (idx_l == len_l) {
         while (idx_r < len_r) {
-            sorted[idx_sorted] = right[idx_r];
+            left[idx_sorted] = right_copy[idx_r];
             idx_r++;
             idx_sorted++;
         } 
     } else {
         while (idx_l < len_l) {
-            sorted[idx_sorted] = left[idx_l];
+            left[idx_sorted] = left_copy[idx_l];
             idx_l++;
             idx_sorted++;
         }
     }
     for (idx_sorted = 0; idx_sorted < len_l + len_r; idx_sorted++) {
-        left[idx_sorted] = sorted[idx_sorted];
+        left_copy[idx_sorted] = left[idx_sorted];
     }
     return;
 }
 
-void merge_sort(int array[], int length) {
+void merge_sort_copy(int array[], int copy[], int length) {
     if (length == 1) {
         return;
     } else {
         int* left = array;
+        int* left_copy = copy;
         int len_l = length / 2;
         int* right = array + (length / 2);
-        int len_r  = (length / 2) + (length % 2);
-        merge_sort(left, len_l);
-        merge_sort(right, len_r);
-        merge(left, len_l, right, len_r);
+        int* right_copy = copy + (length / 2);
+        int len_r  = length - (length / 2);
+        merge_sort_copy(left, left_copy, len_l);
+        merge_sort_copy(right, right_copy, len_r);
+        merge(left, left_copy, len_l, right, right_copy, len_r);
         return;
     }
+}
+
+void merge_sort(int array[], int length) {
+    int i;
+    int copy[length];
+    for (i = 0; i < length; i++) {
+        copy[i] = array[i];
+    }
+    merge_sort_copy(array, copy, length);
+    return;
 }
 
 int main() {
